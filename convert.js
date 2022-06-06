aaebb6fb9b0c20022c641e4296fb8569898a0992
Tyler D <tyler@tdaigle.com>
2022-06-06 14:55:02 -0400

    fixed images/ directory and added <main> tag

--- convert.js
+++ convert.js
@@ -1,24 +1,29 @@
+const { exec } = require("child_process");
+
 const fs = require("fs/promises");
 const showdown = require("showdown");
-
-// const md = `
-// ![linux](https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black) ![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black) ![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) ![CSS](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white) ![React](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB) ![Figma](https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white) ![Git](https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white)
-// `;
-
-// const html = converter.makeHtml(md);
-// console.log(html);
+const deployDirectory = "./dist";
+const stylesheetDirectory = "./styles";
+const imageDirectory = "./images";
+const outputFileName = "index.html";
 
 async function createHtml() {
   const converter = new showdown.Converter();
   converter.setOption("noHeaderId", true);
+
+  // read the markdown and convert it to html
   const data = await fs.readFile("portfolio.md", { encoding: "utf-8" });
   const portfolioData = converter.makeHtml(data);
 
   const header = await loadTemplate("./templates/header.template");
   const footer = await loadTemplate("./templates/footer.template");
+
+  // the html string to write to the file
   const html = header + portfolioData + footer;
 
   console.log(html);
+  await copyStylesheet();
+  await copyImages();
 }
 
 async function loadTemplate(templateFileName) {
@@ -27,4 +32,32 @@ async function loadTemplate(templateFileName) {
   });
   return templateData;
 }
+
+async function copyImages() {
+  return new Promise((resolve, reject) => {
+    exec(`cp -R ${imageDirectory} ${deployDirectory}`, (err, output) => {
+      if (err) {
+        reject(output);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+}
+
+async function copyStylesheet() {
+  return new Promise((resolve, reject) => {
+    exec(
+      `cp -R ${stylesheetDirectory} ${deployDirectory}/${stylesheetDirectory}`,
+      (err, output) => {
+        if (err) {
+          reject(output);
+        } else {
+          resolve(true);
+        }
+      }
+    );
+  });
+}
+
 createHtml();
